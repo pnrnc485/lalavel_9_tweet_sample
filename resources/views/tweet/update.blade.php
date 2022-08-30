41b4744a2ff24b653e6202710b1bdb43323dd4e5
Kenta Suzuki <kenta.suzuki1014@gmail.com>
2022-08-30 17:40:58 +0900

    feat: 編集画面作成

---
+++ resources/views/tweet/update.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>つぶやきアプリ</title>
+    </head>
+<body>
+    <h1>つぶやきを編集する</h1>
+    <div>
+        <a href="{{ route('tweet.index') }}"> 戻る</a>
+        <p>編集フォーム</p>
+        <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
+            @method('PUT')
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140文字まで</span>
+            <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力">{{ $tweet->content }}</textarea>
+            {{-- エラーバリデーションを表示 --}}
+            @error('tweet')
+            <p style="color: red;">{{ $message }}</p>
+            @enderror
+            <button type="submit">編集</button>
+        </form>
+    </div>
+</body>
+</html>
